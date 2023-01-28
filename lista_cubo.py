d1e0083b67740a34d84b284dc7e4cf04975951bb
Valerie Calvo <viorelmcs@gmail.com>
2023-01-27 20:38:42 -0600

    change

---
+++ lista_cubo.py
@@ -0,0 +1,10 @@
+#Lista al cubo: Escriba un programa que cree una lista de números y la
+#imprima. Luego debe imprimir dicha lista pero con cada valor convertido en
+#su cubo.
+
+lista = []
+
+for i in range (5):
+    numeros=int(input("Ingrese 5 números: "))
+    lista.append(numeros **3)
+print (lista)
--- strings_intercaladas.py
+++ strings_intercaladas.py
@@ -1,3 +1,7 @@
+#Strings intercaladas: Escriba un programa que reciba dos strings del mismo
+#largo por parte del usuario e imprima una nueva string con los caracteres de
+#ambos inputs intercalados.
+
 print ("Ingrese la primera palabra: ")
 primera = input()
 
