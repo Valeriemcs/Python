20dc1be4a9b5a167a4e73151cf165e082870139b
Valerie Calvo <viorelmcs@gmail.com>
2023-02-14 17:30:09 -0600

    Cambios y Comentarios

--- Tarea4/repetidos.py
+++ Tarea4/repetidos.py
@@ -21,6 +21,7 @@ lista1=[20, 30, 40, 20, 5, 100, 5, 20]
 lista2=["perro", "gato", "perro", "mapache"]
 lista3 =["Arroz", "Patata", "Pato", "Ámbar"]
 lista4= [""]
-# print(sorted(repetidos(lista_numeros)))
-# print(sorted(repetidos(lista_palabras)))
-resultado = repetidos(lista4)
+
+#Llama a la función
+#cambiar a lista1, lista2, lista3 y lista4 dentro del parentesis para hacer las pruebas.
+resultado = repetidos(lista1)
